a18d7866c2e3fec6c07c2a0cae2abe991e25e3f0
Nguyễn Vĩnh Tài <rivenmerryme123@gmail.com>
2026-03-25 14:10:34 +0700

    add transform to Driver.cs

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -11,6 +11,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, 0.1f);
     }
 }
