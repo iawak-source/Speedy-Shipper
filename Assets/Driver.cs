3ebd1957cd1b421119711b8a9fb1b5d44d7fecfc
Nguyễn Vĩnh Tài <rivenmerryme123@gmail.com>
2026-04-25 10:25:57 +0700

    add KeyPress and Time.DeltaTime

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,17 +1,45 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float steerSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.05f;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0.01f, 0);
-        transform.Rotate(0, 0, 0.1f);
+        float move = 0f;
+        float steer = 0f;
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmout = moveSpeed * move * Time.deltaTime;
+        float steerAmout = steerSpeed * steer * Time.deltaTime;
+
+        transform.Rotate(0, 0, steerAmout);
+        transform.Translate(0, moveAmout, 0);
+
     }
 }
