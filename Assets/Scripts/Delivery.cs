bc3b30f81d7da90b37e3dc3ec30e71173d8a5e1a
Nguyễn Vĩnh Tài <rivenmerryme123@gmail.com>
2026-05-03 16:45:15 +0700

    add some texture to know if the player has package or not

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,24 +1,36 @@
-using System;
-using NUnit.Framework;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
     [SerializeField] float delay = 1f;
+
+    void Start()
+    {
+        GetComponent<ParticleSystem>().Stop();
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package") && !hasPackage)
         {
-            Debug.Log("Pick up Package");
-            hasPackage = true;
-            Destroy(collision.gameObject, delay);
+            if (collision.CompareTag("Package") && !hasPackage)
+            {
+                Debug.Log("Pick up Package");
+                hasPackage = true;
+                GetComponent<ParticleSystem>().Play();
+                Destroy(collision.gameObject, delay);
+
+            }
+            else
+            {
+                GetComponent<ParticleSystem>().Stop();
+            }
         }
         if (collision.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered Package");
             hasPackage = false;
-            Destroy(collision.gameObject, delay);
+            GetComponent<ParticleSystem>().Stop();
         }
     }
 
